dc83c51ed919333cf083b5f0ad76bc9558b4b216
SJTGSHIVAM <getlovebymagicalshivam@gmail.com>
2022-04-14 16:26:00 +0530

    add: footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,37 @@
-export const Footer = () => <>footer</>;
+import {
+  FaBriefcase,
+  FaGithub,
+  FaLinkedinIn,
+  FaMedium,
+} from 'react-icons/fa';
+
+export const Footer = () => (
+  <footer className={""}>
+    <ul>
+      <li className="">
+        <a href="https://github.com/SJTGSHIVAM" target="blank">
+          <FaGithub />
+        </a>
+      </li>
+      <li className="">
+        <a href="https://sjtgshivam.medium.com" target="blank">
+          <FaMedium />
+        </a>
+      </li>
+      <li className="">
+        <a
+          href="https://www.linkedin.com/in/shivam-pandey-766524161"
+          target="blank"
+        >
+          <FaLinkedinIn />
+        </a>
+      </li>
+      <li className="">
+        <a href="https://sjtgshivam.netlify.app/" target="blank">
+          <FaBriefcase />
+        </a>
+      </li>
+    </ul>
+    <div className="">© 2021 | Shivam Pandey</div>
+  </footer>
+);
